4262a2cc77e925b6ec10c07c176141cad5de9053
mateuszkor <mateuszkor5006@gmail.com>
2023-07-18 17:23:06 +0200

    Updated footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -11,20 +11,23 @@ function Footer() {
   return (
     <div className="py-5 text-center">
       <p className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8">
-        {outro.text}
-        {/* <a
-          href="https://www.fiverr.com/s2/3001fc81b1"
+        {outro.text}.
+      </p>
+
+      <p className="text-sm mt-2  opacity-50">
+        &copy; {new Date().getFullYear()} {outro.name}. All rights reserved.
+      </p>
+
+      <p className="text-sm mt-2  opacity-50">
+        Return to {''}
+        <a
+          href="https://optiminium.org/"
           target="_blank"
           rel="noopener noreferrer"
           style={linkStyle}
         >
-          {' '}
-          here
-        </a>
-        . */}
-      </p>
-      <p className="text-sm mt-2  opacity-50">
-        &copy; {new Date().getFullYear()} {outro.name}. All rights reserved.
+          Optiminium.org
+        </a>{' '}
       </p>
     </div>
   );
